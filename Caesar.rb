308114cf55adfc1f48f35b623d18bf0c48a485cc
Glaaki <glaaki@protonmail.com>
2018-02-19 16:29:58 +0100

    Cypher 1 working

--- Caesar.rb
+++ Caesar.rb
@@ -1,6 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+def cypher(inputString)
+  abc = "abcdefghijklmnopqrstuvwxyz"
+  if inputString == nil
+    return ""
+  end
+
+  for pos in 0...inputString.length
+    letterIn = inputString[pos].downcase
+    if abc.include?(letterIn)
+      indexIn = abc.index(letterIn)
+      indexOut = (indexIn + 1) % 26
+      inputString[pos] = (inputString[pos] == letterIn) ? abc[indexOut] : abc[indexOut].upcase
+    end
+  end
+
+
+  return inputString
+end
+
 get '/' do
-  "Hello Caesar"
+  inputString = params["inputString"]
+  erb :index, :locals => {:inputString => cypher(inputString)}
 end
